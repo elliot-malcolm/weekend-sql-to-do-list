4093bb93007a440aa150f65fb04dee379dd07f81
Elliot Malcolm <talcumalc@gmail.com>
2020-10-18 22:54:45 -0500

    initial commit

---
+++ server/modules/pool.js
@@ -0,0 +1,20 @@
+const pg = require('pg');
+
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 
+    host: 'localhost',
+    port: 5432,
+    max: 10, 
+    idleTimeoutMillis: 30000 
+});
+
+pool.on('connect', () => {
+    console.log("Postgresql Connected");
+} );
+
+pool.on('error', (error) => {
+    console.log("error with Postgresl pool", error);
+});
+
+module.exports = pool;
