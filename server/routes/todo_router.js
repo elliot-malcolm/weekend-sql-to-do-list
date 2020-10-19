d10e6a4c869ca9693afa40e282cf53ebb739fcc0
Elliot Malcolm <talcumalc@gmail.com>
2020-10-19 00:07:10 -0500

    get post routerside

--- server/routes/todo_router.js
+++ server/routes/todo_router.js
@@ -5,15 +5,26 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     console.log("in get");
-    res.sendStatus(200)
+    let queryText = `SELECT * FROM "to_do" ORDER BY "id"`;
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error)=>{
+        console.log("error with get request", error);
+        res.sendStatus(500);
+    });
 });
-    // let queryText = `SELECT * FROM "songs" ORDER BY "id"`;
-    // pool.query(queryText).then((result) => {
-    //     res.send(result.rows);
-    // }).catch((error)=>{
-    //     console.log("error with get request", error);
-    //     res.sendStatus(500);
-    // });
-// });
+
+router.post('/', (req, res)=>{
+    let toDoItem = req.body;
+    let queryText = `INSERT INTO "to_do"("list_item")
+    VALUES($1);`;
+    pool.query(queryText, [toDoItem.listItem])
+    .then((result)=>{
+        console.log('back from POST pg,', result.rows);
+        res.sendStatus(200);
+    }).catch((err)=>{
+        console.log('error in POST pg', err);
+        res.sendStatus(500);
+    })
 
 module.exports = router;
